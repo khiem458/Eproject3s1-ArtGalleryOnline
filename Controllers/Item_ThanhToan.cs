15bcae560799e449efca5ce4e2eabd258f87713c
qui281194 <quicao902@gmail.com>
2023-07-26 05:25:31 +0700

    chan qua lam hok dc

---
+++ Controllers/Item_ThanhToan.cs
@@ -0,0 +1,16 @@
+﻿using ArtGalleryOnline.Infrastructure;
+using ArtGalleryOnline.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGalleryOnline.Controllers
+{
+    public class Item_ThanhToan: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            
+            return View("Item_ThanhToan", HttpContext.Session.GetJson<Cart>("cart"));
+        }
+       
+    }
+}
