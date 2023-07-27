c95bd3358828bd04a4862804ac40962721775fc9
qui281194 <quicao902@gmail.com>
2023-07-27 16:57:26 +0700

    Upload model Contact

---
+++ Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtGalleryOnline.Models
+{
+    public class Contact
+    {
+        [Key]
+        public string? ContactName { get; set; }
+        [Required]
+        public string? ContactTitle { get; set; }
+        [Required]
+        public string? ContactEmail { get; set; }
+        [Required]
+        public string? ContactPhone { get; set; }
+        [Required]
+        public string? Description { get; set; }
+    }
+}
