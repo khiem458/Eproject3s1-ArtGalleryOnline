f1ba6230df3adcf7482ac98fd6b3d24da723b188
qui281194 <quicao902@gmail.com>
2023-08-05 21:57:48 +0700

    Upload Admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,15 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ArtGalleryOnline.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArtGalleryOnline.Controllers
 {
     [Authorize(Roles ="admin")]
     public class AdminController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly ArtgalleryDbContext _context;
+
+        public AdminController(ArtgalleryDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var artgalleryDbContext = _context.Orders.Include(o => o.User);
+            return View(await artgalleryDbContext.ToListAsync());
+        }
+        
     }
 }
