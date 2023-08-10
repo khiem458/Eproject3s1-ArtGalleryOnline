d9cd31a81abec5cd62bf79f64d8ab650b483eec0
codecualinh <vythianhlinh02@gmail.com>
2023-08-10 11:15:17 +0700

    upload

---
+++ Models/Gallery.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtGalleryOnline.Models
+{
+    public class Gallery
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int galleryId { get; set; }
+        [StringLength(50)]
+        public string? Image { get; set; }
+    }
+}
