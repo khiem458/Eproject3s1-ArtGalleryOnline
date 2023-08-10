bcaab2e52660b45519eee7441ab2ddbe7c51f21f
qui281194 <quicao902@gmail.com>
2023-08-10 13:24:23 +0700

    Upload trang lịch sử User

---
+++ Controllers/OrderDetailsUser.cs
@@ -0,0 +1,22 @@
+﻿using ArtGalleryOnline.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtGalleryOnline.Controllers
+{
+    public class OrderDetailsUser: ViewComponent
+    {
+        private readonly ArtgalleryDbContext _context;
+
+        public OrderDetailsUser(ArtgalleryDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var artgalleryDbContext = _context.OrderDetails.Include(o => o.ArtWork).Include(o => o.Orders)
+                 .ToList();
+            return View("OrderDetailsUser", artgalleryDbContext);
+        }
+    }
+}
