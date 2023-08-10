7f1cdf22aa4ba8e5bf8c6b034f85704a45cf7083
qui281194 <quicao902@gmail.com>
2023-08-10 10:38:07 +0700

    Upload

--- Controllers/OrderDetailsController.cs
+++ Controllers/OrderDetailsController.cs
@@ -11,7 +11,7 @@ using System.Data;
 
 namespace ArtGalleryOnline.Controllers
 {
-    [Authorize(Roles = "admin")]
+   
     public class OrderDetailsController : Controller
     {
         private readonly ArtgalleryDbContext _context;
